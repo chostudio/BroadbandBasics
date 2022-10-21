ac1eadcfc03178e23607daeac6bb58768048248f
Minecraft-Cake <96643025+Minecraft-Cake@users.noreply.github.com>
2022-10-20 16:36:51 -1000

    DOES NOT WORK added quiz javascript file, progress bar, and tried to add require.js

---
+++ quiz.js
@@ -0,0 +1,41 @@
+requirejs(["helper/util"], function(util) {
+
+
+
+});
+
+
+var Bar = {
+    element: document.getElementById("progressBar"),
+
+    set progress(value) {
+        this.element.style.width = value + "%";
+    },
+    get progress() {
+        return this.element.style.width
+    },
+
+}
+
+Bar.progress = 10
+const answers = require('./answers/lesson_1.json'); 
+
+console.log(answers)
+
+function selectAnswer(id) {
+    switch (id) {
+        case 0: {
+            
+        }
+    }
+}
+
+/*
+object.property
+object.getProperty
+
+object.property = 1
+object.setProperty(1)
+
+
+*/
