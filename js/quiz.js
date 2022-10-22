84bac03489dd1a9ca5ad778f12a3f5921823bf92
Minecraft-Cake <96643025+Minecraft-Cake@users.noreply.github.com>
2022-10-22 13:51:20 -1000

    Made a functional ish quiz I have to go right now and don't have anymore time, but for now 1. Implement the json reader (I put that off) 2. Look for //TODO s
    dang, I am so proud of this

--- js/quiz.js
+++ js/quiz.js
@@ -1,10 +1,6 @@
-requirejs(["helper/util"], function(util) {
-
-
-
-});
-
-
+"use strict";
+import * as Utilities from './utils.js';
+const Utils = Utilities.default
 var Bar = {
     element: document.getElementById("progressBar"),
 
@@ -16,26 +12,105 @@ var Bar = {
     },
 
 }
-
-Bar.progress = 10
-const answers = require('../answers/lesson_1.json'); 
-
-console.log(answers)
-
-function selectAnswer(id) {
-    switch (id) {
-        case 0: {
-            
-        }
+async function fetchAnswers(id) {
+    try {
+        const response = await fetch(`./answers/lesson_${id}.json`);
+        const exam = await response.json();
+        return exam;    
+    } catch (error) {
+        console.error(error);
     }
 }
 
-/*
-object.property
-object.getProperty
+const answers = {
+    "0": {
+        "name": "click the cheese",
+        "description": "description",
+        "options": {
+            "Bacon": false,
+            "Bread": false,
+            "Milk": false,
+            "Cheese": true
+        }  
+    },
+    "1": {
+        "name": "click yes",
+        "description": "description again",
+        "options": {
+            "Nope": false,
+            "No": false,
+            "I refuse": false,
+            "Yes": true
+        }  
+    }
+}
+//init variables
+var selectedAnswers = []
+var options;
+var questions = Object.keys(answers).length;
+var amountToUpdate = 100 / questions
+var currentQuestionNum = 0;
 
-object.property = 1
-object.setProperty(1)
+//init functions
+function update(questionNum) {
+    console.log(questionNum);
+    currentQuestionNum = questionNum;
+    options = Object.keys(answers[currentQuestionNum.toString()]['options']);
+    Utils.shuffle(options);
+    console.log(options);
 
 
-*/
+    Array.from(document.getElementsByClassName('answerChoice')).forEach(element => {
+        //TODO find a less stupid solution
+        Utils.replaceButtonText(element, options[element.id.substring(12)])
+        //for the sake of D R Y
+        element.disabled = false;
+    });
+
+}
+update(0)
+
+registerAnswerChoiceButton(0);
+registerAnswerChoiceButton(1);
+registerAnswerChoiceButton(2);
+registerAnswerChoiceButton(3);
+
+function registerAnswerChoiceButton(number) {
+    const button = document.getElementById(`answerChoice${number}`)
+    button.addEventListener("click", selectAnswer, false)
+    button.number = number;
+
+}
+function selectAnswer(event) {
+    let selectedOption = options[event.currentTarget.number]
+
+    //disable the buttons to prevent spamming
+    Array.from(document.getElementsByClassName('answerChoice')).forEach(element => {
+        element.disabled = true;
+    });
+
+    if (answers[currentQuestionNum]['options'][selectedOption]) {
+        //TODO display correct answer animation
+        console.log("correct!")
+        
+    } else {
+        //TODO display incorrect answer animation
+        console.log("incorrect!")
+    }
+    
+    Bar.progress += amountToUpdate;
+    selectedAnswers.push(selectedOption);
+
+    if (currentQuestionNum + 1 === questions) {
+        //finish
+        console.log("FINISHED")
+        //TODO display answers
+        console.log(selectedAnswers)
+        return
+    } 
+
+    update(currentQuestionNum + 1)
+
+
+}
+
