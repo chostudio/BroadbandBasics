dc5def35f8a0f4cc08881a29bc6f576619a18d56
aaron broderick <brodericka24@maryknollschool.org>
2022-10-26 15:23:02 -1000

    Commit

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 //comment
 
+document.getElementById('button').addEventListener("click", function(){
+    document.getElementById('overlay').style.display = "block";
+    document.getElementById('overlay2').style.display = "none";
+    document.getElementById('correct').style.display = "inline-block";
+});
+
+document.getElementById('button2').addEventListener("click", function(){
+    document.getElementById('overlay').style.display = "none";
+    document.getElementById('overlay2').style.display = "block";
+    document.getElementById('incorrect').style.display = "inline-block";
+});    
